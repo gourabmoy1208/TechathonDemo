03a6495936f94d1bdfb22976fafb812c4276566f
Gourabmoy Ghosh <gourabmoyghosh@in.ibm.com>
2014-11-22 15:34:29 +0530

    Commit the code for Techathon 2014.

--- TechathonDemoApplication/apps/TechathonDemo/android/native/assets/www/default/worklight/checksum.js
+++ TechathonDemoApplication/apps/TechathonDemo/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":1835172119,"date":1416643900904,"machine":"ADMINIB-P1DALNR"};
-/* Date: Sat Nov 22 13:41:40 IST 2014 */
+var WL_CHECKSUM = {"checksum":1351440403,"date":1416649942728,"machine":"ADMINIB-P1DALNR"};
+/* Date: Sat Nov 22 15:22:22 IST 2014 */
--- TechathonDemoApplication/apps/TechathonDemo/iphone/native/www/default/worklight/checksum.js
+++ TechathonDemoApplication/apps/TechathonDemo/iphone/native/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":3037126399,"date":1416643888952,"machine":"ADMINIB-P1DALNR"};
-/* Date: Sat Nov 22 13:41:28 IST 2014 */
+var WL_CHECKSUM = {"checksum":4000017758,"date":1416649952731,"machine":"ADMINIB-P1DALNR"};
+/* Date: Sat Nov 22 15:22:32 IST 2014 */
